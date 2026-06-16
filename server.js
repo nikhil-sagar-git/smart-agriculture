5efe31be281cf89e7fa945a7ffc04806bd52f980
R Dinesh <rasalanikhil@gmail.com>
2026-06-16 11:00:03 +0530

    updated server

--- server.js
+++ server.js
@@ -44,11 +44,32 @@ db.connect((err) => {
 const otpStore = {};
 
 // 📧 Mail setup
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   }
+// });
+
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+
+  connectionTimeout: 10000,
+  socketTimeout: 10000
+});
+
+transporter.verify((err) => {
+  if (err) {
+    console.log("MAIL ERROR:", err.message);
+  } else {
+    console.log("MAIL READY");
   }
 });
 
