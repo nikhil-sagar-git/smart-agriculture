b6f4321d35114c070f22c8ae16ff5e312fc37c15
R Dinesh <rasalanikhil@gmail.com>
2026-07-17 11:50:30 +0530

    updated server

--- server.js
+++ server.js
@@ -324,16 +324,10 @@ const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
-
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000
+  }
 });
 
 transporter.verify((err) => {
